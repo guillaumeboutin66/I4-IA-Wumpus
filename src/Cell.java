ca66ea1c599f9412fa261fd5a9a0045dfc01485b
Franofarion <baptiste.domange@gmail.com>
2017-12-14 14:55:09 +0100

    Creation GameMap & Cell

---
+++ src/Cell.java
@@ -0,0 +1,11 @@
+public class Cell {
+
+    // todo : abstract
+    int x;
+    int y;
+
+    // todo : put a number ?
+    Cell(int x, int y, String severity){
+
+    }
+}
--- src/GameMap.java
+++ src/GameMap.java
@@ -1,2 +1,42 @@
+
+
 public class GameMap {
+
+    private int width;
+    private int length;
+    private Cell[][] cells;
+
+    GameMap(int w, int l){
+        setWidth(w);
+        setLength(l);
+
+        this.cells = new Cell[getWidth()][getLength()];
+
+        //Wumpus
+        int xWumpus = generateRandom(getWidth());
+        int yWumpus = generateRandom(getLength());
+        Cell wumpus = new Cell(xWumpus,yWumpus,"wumpus");
+        this.cells[xWumpus][yWumpus] = wumpus;
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    private int generateRandom(int Max){
+        return (int) Math.round(1 + (Math.random() * (Max - 1)));
+    }
 }
