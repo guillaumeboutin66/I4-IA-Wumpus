7062c609e1f7d16809088d0cee15238c100ec84a
azuron <azuron@hotmail.fr>
2018-02-21 23:03:25 +0100

    Basic movement

--- src/main/ComputedDecision.java
+++ src/main/ComputedDecision.java
@@ -1,6 +1,7 @@
 package main;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -8,24 +9,89 @@ import java.util.Random;
  */
 public class ComputedDecision {
 
-
     Random randomPeer = new Random();
-    Cell[][] knownCases;
+    SuspiciousCell[][] knownCases;
     Cell[][] map;
     Point playerPosition;
 
-    public ComputedDecision(Cell[][] map, Cell[][] knownCases, Point playerPosition){
+    //SuspiciousCell
+    public ComputedDecision(Cell[][] map, SuspiciousCell[][] knownCases, Point playerPosition){
         this.map = map;
         this.knownCases = knownCases;
         this.playerPosition = playerPosition;
     }
 
-    private Direction TakeRandomDecision(){
-        int rand =  randomPeer.nextInt(4);
-        return Direction.values()[rand];
+
+    public Direction[] takeDecision(){
+        //return RandomDecision();
+        return goTo(BasicComputedDecision());
+    } 
+
+    private SuspiciousCell BasicComputedDecision(){
+        ArrayList<SuspiciousCell> cells = new ArrayList<SuspiciousCell>();
+        cells.add(knownCases[playerPosition.x][playerPosition.y+1]);
+        cells.add(knownCases[playerPosition.x][playerPosition.y-1]);
+        cells.add(knownCases[playerPosition.x+1][playerPosition.y]);
+        cells.add(knownCases[playerPosition.x-1][playerPosition.y]);
+        Collections.shuffle(cells);
+
+        SuspiciousCell choosedCell = cells.get(0);
+
+        /* On priorise les cases non découvertes */
+        for (SuspiciousCell cell : cells) {
+            if(cell.safeLevel == SuspiciousCell.SafeLevel.unknown){
+                if(choosedCell.safeLevel != SuspiciousCell.SafeLevel.unknown){
+                    cell = choosedCell;
+                }else if(cell.danger < choosedCell.danger){
+                    cell = choosedCell;
+                }
+            }
+        }
+
+        if(choosedCell.safeLevel == SuspiciousCell.SafeLevel.unknown){
+            return choosedCell; //On a trouvé une celle où aller
+        }
+
+        /* On a pas trouvé une case inconnu autour de soit */
+        for (SuspiciousCell cell : cells) {
+            if(cell.safeLevel == SuspiciousCell.SafeLevel.safe){
+                return cell;
+            }
+        }
+
+        for (SuspiciousCell cell : cells) {
+            if(cell.safeLevel == SuspiciousCell.SafeLevel.unsafe){
+                return cell;
+            }
+        }
+
+        return choosedCell;
     }
 
-    public Direction ComputedDecision(){
+    private Direction[] goTo(SuspiciousCell cell){
+        ArrayList<Direction> direction = new ArrayList<Direction>();
+        if(cell.position.y > playerPosition.y){
+            direction.add(Direction.up);
+        }else if(cell.position.y < playerPosition.y){
+            direction.add(Direction.down);
+        }else if(cell.position.x > playerPosition.x){
+            direction.add(Direction.right);
+        }else{
+            direction.add(Direction.left);
+        }
+
+        return direction.toArray(new Direction[direction.size()]);
+    }
+
+    private Direction[] RandomDecision(){
+        int rand =  randomPeer.nextInt(4);
+        Direction[] directions = new Direction[1];
+        directions[0] = Direction.values()[rand];
+        return directions;
+    }
+
+    /*
+    public ArrayList<Direction> ComputedDecision(){
 
         SafeLevel caseUp = CheckIfCaseIsSafe(playerPosition.x, playerPosition.y+1);
         SafeLevel caseDown = CheckIfCaseIsSafe(playerPosition.x, playerPosition.y-1);
@@ -35,31 +101,38 @@ public class ComputedDecision {
         return TakeRandomDecision();
     }
 
+    private ArrayList<Direction> TakeBasicDecision(SafeLevel[] safeLevels){
+        ArrayList<SafeLevel> safe = new ArrayList<SafeLevel>();
+        ArrayList<SafeLevel> unknown = new ArrayList<SafeLevel>();
+
+        
+        for (int i = 0; i < safeLevels.length; i++) {
+            
+        }
+    }
 
     public SafeLevel CheckIfCaseIsSafe(int x, int y){
         if(x < -1){ //Si on sait qu'on sort du map connu
-            return SafeLevel.notReachable;
+            return new SafeLevel(SafeLevel.Level.notReachable, x, y);
         }
 
         if(y > map[0].length){ //Si on sait qu'on sort du map connu
-            return  SafeLevel.notReachable;
+            return new SafeLevel(SafeLevel.Level.notReachable, x, y);
         }
         try{//Le try permet de s'assurer que si on tape dans
             Cell tmp = knownCases[x][knownCases[0].length -y];
             if(tmp!= null){
                 if(tmp.getEvents().size() == 0){
-                    return SafeLevel.safe;
+                    return new SafeLevel(SafeLevel.Level.safe, x, y);
                 }else{
-                    return SafeLevel.unsafe;
+                    return new SafeLevel(SafeLevel.Level.unsafe, x, y);
                 }
             }
-            return SafeLevel.unknown;
+            return new SafeLevel(SafeLevel.Level.notReachable, x, y);
         }catch (Exception ex){
-            return  SafeLevel.unknown;
+            return new SafeLevel(SafeLevel.Level.unknown, x, y);
         }
-    }
-
-
+    }*/
 
     public enum Direction{
         up,
@@ -67,13 +140,5 @@ public class ComputedDecision {
         right,
         left
     }
-
-    public enum SafeLevel{
-        safe,
-        unsafe,
-        unknown,
-        notReachable
-    }
-
 
 }
