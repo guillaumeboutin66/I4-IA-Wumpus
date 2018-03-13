72e9e83ffbfb06a2cce3725a781a4656ca7d8bc5
Azuron <azuron@hotmail.fr>
2018-03-13 11:52:32 +0100

    star search

--- src/main/SuspiciousCell.java
+++ src/main/SuspiciousCell.java
@@ -10,10 +10,20 @@ public class SuspiciousCell {
     Point position;
     int danger = 0;
     
+    SuspiciousCell(Point pos, SafeLevel level){
+        position = pos;
+        safeLevel = level;
+    }
+
     public enum SafeLevel{
         safe,
         unsafe,
         unknown,
         notReachable
+    }
+
+    @Override
+    public String toString() {
+        return "Suspicious cell :\n Position : x " + this.position.x + "| y " + this.position.y + "\n Safelevl : " +this.safeLevel.toString();
     }
 }
